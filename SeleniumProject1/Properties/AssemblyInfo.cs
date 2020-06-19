18cd33c9c46f079f0e5e5478131e3ea6f6663528
DJ <Dhananjai.Misra@pb.com>
2020-06-19 16:41:54 +0530

    First commit to the Selenium C# Project Repo

---
+++ SeleniumProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("047ef6a9-dc05-47de-9dad-0e03fdb9c9a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumProject1/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace SeleniumProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = GetChromeDriver();
+            driver.Navigate().GoToUrl("http://internal.qa1fxwebas1.newgistics.com/");
+            //Driver Navigation Functions
+            //driver.Navigate().Back();
+            //driver.Navigate().Forward();
+            //driver.Navigate().Refresh();
+
+            // Using implicit waits
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+            //Using Explicit wait
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+
+            //Driver Interrogation
+            Console.WriteLine(driver.CurrentWindowHandle);
+            Console.WriteLine(driver.WindowHandles);
+            Console.WriteLine(driver.PageSource);
+            Console.WriteLine(driver.Title);
+            Console.WriteLine(driver.Url);
+
+            var pwdField = driver.FindElement(By.Id("SecureIDTextBox"));
+            Assert.IsTrue(pwdField.Displayed);
+            Assert.IsTrue(pwdField.Enabled);
+            Assert.IsFalse(pwdField.Selected);
+            pwdField.Clear();
+            pwdField.SendKeys("devbadge");
+            pwdField.SendKeys(Keys.Enter);
+
+            var productSKU = wait.Until((d) =>
+            {
+                return d.FindElement(By.Id("ctl00_ProductLookupTextBox_inputTextBox"));
+            });
+            Assert.IsTrue(productSKU.Displayed);
+        }
+
+        private IWebDriver GetChromeDriver()
+        {
+            var outputDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outputDir);
+        }
+    }
+}
