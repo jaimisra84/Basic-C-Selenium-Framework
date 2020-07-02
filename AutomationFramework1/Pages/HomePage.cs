48a743cabc5fb5fb7f57d9b0527d68e700fd470d
DJ <Dhananjai.Misra@pb.com>
2020-07-02 16:05:05 +0530

    Developing the NLog Utility for the framework

--- AutomationFramework1/Pages/HomePage.cs
+++ AutomationFramework1/Pages/HomePage.cs
@@ -1,10 +1,13 @@
-﻿using OpenQA.Selenium;
+﻿
+using NLog;
+using OpenQA.Selenium;
 
 namespace AutomationFramework1.Pages
 {
     internal class HomePage : BasePage
     {
         //WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+        private static Logger log = LogManager.GetCurrentClassLogger();
         public IWebElement ProductSearchTesxtBox => Driver.FindElement(By.Id("ctl00_ProductLookupTextBox_inputTextBox"));
         public IWebElement ProductSearchButton => Driver.FindElement(By.Id("ctl00_ProductSearchButton"));
         private string PageTitle => "Newgistics WMS :: Home";
@@ -16,6 +19,7 @@ namespace AutomationFramework1.Pages
         {
             ProductSearchTesxtBox.SendKeys(productID);
             ProductSearchButton.Click();
+            log.Info($"Searching the product: {productID}");
             return new ProductPage(Driver);
         }
     }
--- AutomationFramework1/Pages/LoginPage.cs
+++ AutomationFramework1/Pages/LoginPage.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
 using OpenQA.Selenium;
 using System;
 
@@ -6,6 +7,7 @@ namespace AutomationFramework1.Pages
 {
     internal class LoginPage : BasePage
     {
+        private static Logger log = LogManager.GetCurrentClassLogger();
         public IWebElement SecureIDTesxtBox => Driver.FindElement(By.Id("SecureIDTextBox"));
         public bool IsVisible => Driver.Title.Equals(PageTitle);
         /*public bool IsVisible { 
@@ -21,14 +23,17 @@ namespace AutomationFramework1.Pages
 
         internal void Goto()
         {
-            Driver.Navigate().GoToUrl("http://internal.qa1fxwebas1.newgistics.com/");
+            var url = "http://internal.qa1fxwebas1.newgistics.com/";
+            Driver.Navigate().GoToUrl(url);
             Assert.IsTrue(IsVisible, "Not able to validate the title of the LOGIN PAGE");
+            log.Info($"Opening the URL: {url}");
         }
 
         internal HomePage Login(string userName)
         {
             SecureIDTesxtBox.SendKeys(userName);
             SecureIDTesxtBox.Submit();
+            log.Info($"Entering the username: {userName} and clicking on submit");
             return new HomePage(Driver);
         }
     }
